ab5633ce442bdf7b34a2b0abfc4e0091aa7bd2f2
Maryam Jangjoo <mrymjangjoo@gamil.com>
2026-07-11 14:09:51 +0330

    chore: add missing main.ts and platform files

---
+++ backend-api/src/application/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, 
+    transform: true, 
+  }));
+
+  const config = new DocumentBuilder()
+    .setTitle('ECO-SMART API')
+    .setDescription('Official API documentation for ECO-SMART project')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+  console.log(`🚀 ECO-SMART Server is running on: http://localhost:3000`);
+}
+bootstrap();
